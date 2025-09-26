cd5b1cb2138591c4bc12ae8dcdbdc23c463583e7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-09-26 21:20:18 +0100

    pop up menu bottom

--- lib/HomePage/HomePage.dart
+++ lib/HomePage/HomePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key,});
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -25,14 +25,11 @@ class _MyHomePageState extends State<MyHomePage> {
       _taskController.clear();
     });
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Tasks"),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: Text("My Tasks"), centerTitle: true),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -49,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       decoration: InputDecoration(
                         hintText: "Enter a new task",
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15)
+                          borderRadius: BorderRadius.circular(15),
                         ),
                       ),
                       onFieldSubmitted: (_) => _addTask(),
@@ -58,29 +55,30 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 IconButton(
                   icon: Icon(Icons.add),
-                  onPressed: (){
+                  onPressed: () {
                     _addTask();
                   },
-                )
+                ),
               ],
             ),
             Expanded(
               child: ListView.builder(
                 itemCount: _tasks.length,
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   return Dismissible(
                     background: Container(
                       alignment: Alignment.centerRight,
                       color: Colors.red,
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Icon(Icons.delete,color: Colors.white,),
+                        child: Icon(Icons.delete, color: Colors.white),
                       ),
                     ),
                     key: ValueKey(_tasks[index]["id"]),
                     direction: DismissDirection.endToStart,
                     onDismissed: (_) {
-                      final deletedTask = _tasks[index]["title"]; // store before removing
+                      final deletedTask =
+                          _tasks[index]["title"]; // store before removing
                       setState(() {
                         _tasks.removeAt(index);
                       });
@@ -93,7 +91,11 @@ class _MyHomePageState extends State<MyHomePage> {
                             textColor: Colors.white,
                             onPressed: () {
                               setState(() {
-                                _tasks.insert(index, {"id": DateTime.now().millisecondsSinceEpoch, "title": deletedTask, "done": false});
+                                _tasks.insert(index, {
+                                  "id": DateTime.now().millisecondsSinceEpoch,
+                                  "title": deletedTask,
+                                  "done": false,
+                                });
                               });
                             },
                           ),
@@ -102,78 +104,63 @@ class _MyHomePageState extends State<MyHomePage> {
                     },
                     confirmDismiss: (direction) async => true,
                     child: Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
                       child: ListTile(
                         leading: Checkbox(
                           activeColor: Colors.green,
                           value: _tasks[index]["done"],
-                          onChanged: (value){
+                          onChanged: (value) {
                             setState(() {
                               _tasks[index]["done"] = value ?? false;
                             });
                           },
                         ),
-                          title: Text(
-                            _tasks[index]["title"],
-                            style: TextStyle(
-                              fontSize: 16,
-                              decoration: _tasks[index]["done"]
-                                  ? TextDecoration.lineThrough
-                                  : TextDecoration.none,
-                            ),
+                        title: Text(
+                          _tasks[index]["title"],
+                          style: TextStyle(
+                            fontSize: 16,
+                            decoration: _tasks[index]["done"]
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
                           ),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              color: Colors.red,
-                              icon: Icon(Icons.delete),
-                              onPressed: () async {
-                                final deletedTask = _tasks[index]["title"]; // store before removing
-                                await showDialog(
-                                    context: context,
-                                  builder: (ctx) => AlertDialog(
-                                    title: Text("You are about to delete ${_tasks[index]["title"]}"),
-                                    content: Text("Are you sure?", style: TextStyle(fontSize: 18),),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () => Navigator.of(ctx).pop(null),
-                                        child: const Text("Cancel"),
-                                      ),
-                                      TextButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            _tasks.removeAt(index);
-                                          });
-                                          Navigator.of(ctx).pop(null);
-                                          ScaffoldMessenger.of(context).showSnackBar(
-                                            SnackBar(
-                                              content: Text('$deletedTask has been deleted'),
-                                              action: SnackBarAction(
-                                                label: 'Undo',
-                                                textColor: Colors.white,
-                                                onPressed: () {
-                                                  setState(() {
-                                                    _tasks.insert(index, {"id": DateTime.now().millisecondsSinceEpoch, "title": deletedTask, "done": false});
-                                                  });
-                                                },
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                        child: const Text("Delete", style: TextStyle(color: Colors.red),),
-                                      ),
-                                    ],
-                                  ),
-                                );
-
-                              },
-                            ),
-                            SizedBox(width: 6,),
-                            IconButton(
-                              onPressed: () => _editTask(index),
-                              icon: Icon(Icons.edit),
-                            )
-                          ],
+                        ),
+                        trailing: PopupMenuButton<String>(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)
+                          ),
+                          onSelected: (value) async {
+                            if (value == "edit") {
+                              _editTask(index);
+                            }
+                            if (value == "delete") {
+                              final deletedTask =
+                                  _tasks[index]["title"]; // store before removing
+                              await deleteTask(context, index, deletedTask);
+                            }
+                          },
+                          icon: const Icon(Icons.more_horiz),
+                          itemBuilder: (context) {
+                            return [
+                              PopupMenuItem(
+                                value: 'edit',
+                                child: ListTile(
+                                  leading: Icon(Icons.edit),
+                                  title: Text('Edit'),
+                                ),
+                              ),
+                              PopupMenuItem(
+                                value: 'delete',
+                                child: ListTile(
+                                  leading: Icon(Icons.delete),
+                                  title: Text('Delete'),
+                                  iconColor: Colors.red,
+                                ),
+                              ),
+                            ];
+                          },
                         ),
                       ),
                     ),
@@ -181,12 +168,55 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               ),
             ),
-
           ],
         ),
       ),
     );
   }
+
+  Future<dynamic> deleteTask(BuildContext context, int index, deletedTask) {
+    return showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text("You are about to delete ${_tasks[index]["title"]}"),
+        content: Text("Are you sure?", style: TextStyle(fontSize: 18)),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(null),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _tasks.removeAt(index);
+              });
+              Navigator.of(ctx).pop(null);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('$deletedTask has been deleted'),
+                  action: SnackBarAction(
+                    label: 'Undo',
+                    textColor: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        _tasks.insert(index, {
+                          "id": DateTime.now().millisecondsSinceEpoch,
+                          "title": deletedTask,
+                          "done": false,
+                        });
+                      });
+                    },
+                  ),
+                ),
+              );
+            },
+            child: const Text("Delete", style: TextStyle(color: Colors.red)),
+          ),
+        ],
+      ),
+    );
+  }
+
   Future<void> _editTask(int index) async {
     final current = _tasks[index]["title"] as String;
     final controller = TextEditingController(text: current);
@@ -232,10 +262,9 @@ class _MyHomePageState extends State<MyHomePage> {
       });
 
       // Show success message
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Task has been updated'),
-        ),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Task has been updated')));
     }
-  }}
+  }
+}
